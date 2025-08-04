4773726da9e98a9c5ede5aa7735f0d59527dba07
wahe7 <waheguru002200@gmail.com>
2025-08-04 21:35:27 +0530

    adding navbar route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,17 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import CreatePost from './pages/CreatePost';
+import Navbar from './components/navbar';
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/profile/:userId" element={<Profile />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/create" element={<CreatePost />} />
       </Routes>
     </Router>
