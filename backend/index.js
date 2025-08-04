7021ebd99b634c38803dce9d78437655b9e3fa0e
wahe7 <waheguru002200@gmail.com>
2025-08-04 15:50:24 +0530

    update index with user route

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", require("./routes/auth"));
-// app.use("/api/users", require("./routes/users"));
+app.use("/api/users", require("./routes/users"));
 app.use("/api/posts", require("./routes/posts"));
 
 const PORT = process.env.PORT || 3000;
